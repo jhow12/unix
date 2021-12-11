3cd65eca95deb07be9ed69418805d5a08f4a7269
Joha.Pa <95323235+jhow12@users.noreply.github.com>
2021-12-10 23:52:40 -0500

    Update Netsizeall.sh

--- Netsizeall.sh
+++ Netsizeall.sh
@@ -11,9 +11,9 @@ for file in *.txt; do cat $file | wc -l $file; done >> $2
 #Diferenciar las filas y las columnas 
 echo columnas >> $2
 #Se utiliza echo para introducir el texto con el fin de dierenciar el N° de fila y el N° de columna
-for file in *.txt; do head -n1 $file  | grep -o " " $file| wc -l $file; done >> $2
+for file in *.txt; do head -n1 $file  | grep -o " " | wc -l ; done >> $2
 #Se utiliza el bucle for para repetir el comando  para todos los archivos .txt del Directorio Saavedra2013
 #Se usa "head -n1" para tomar la primera fila de los archivos .txt
-#Se usa "grep -o " " "para contar los espacios 
+#Se usa "grep -o " " para contar los espacios 
 #Se usa "wc -l"para contar las lineas 
  
